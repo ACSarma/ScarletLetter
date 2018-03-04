54f2384f05e0f3ca5b1257f1e611cee982360f91
ACSarma <arun.c.sarma@gmail.com>
2018-03-03 23:26:15 -0800

    Ads

--- app/src/main/java/asarmaapps/com/scarletletter/MainActivity.java
+++ app/src/main/java/asarmaapps/com/scarletletter/MainActivity.java
@@ -8,17 +8,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.amazon.device.ads.AdLayout;
+import com.amazon.device.ads.AdRegistration;
+import com.amazon.device.ads.AdTargetingOptions;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button[] button =  new Button[5];
+    private Button[] button = new Button[5];
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
+    private AdLayout adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdRegistration.setAppKey("b0d9a77e4102467cb0a13646904b1dcb");
+        // Programmatically create the AmazonAdLayout
+        /*
+        this.adView = (AdLayout) findViewById(R.id.ad_view);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.mainLayout);
+        // Set the correct width and height of the ad
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        layout.addView(this.adView, lp);
+
+        // If you declared AdLayout in your xml you would instead
+        // replace the 3 lines above with the following line:
+        //   this.adView = (AdLayout) findViewById(R.id.adview);
+
+        AdTargetingOptions adOptions = new AdTargetingOptions();
+        // Optional: Set ad targeting options here.
+        this.adView.loadAd(adOptions); // Retrieves an ad on background thread
+*/
         fragmentManager = getFragmentManager();
         button[0] = findViewById(R.id.home);
         button[1] = findViewById(R.id.description);
@@ -29,49 +55,54 @@ public class MainActivity extends AppCompatActivity {
         GoSome(button[0]);
     }
 
-    public void GoSome(View view){
+    public void GoSome(View view) {
         Fragment fragment;
         fragmentTransaction = fragmentManager.beginTransaction();
-        Button b = (Button)view;
-        switch (b.getText().toString()){
-            case "Home Page": bColor(0);
+        Button b = (Button) view;
+        switch (b.getText().toString()) {
+            case "Home Page":
+                bColor(0);
                 fragment = new Home();
                 fragmentSet(fragmentTransaction, fragment);
                 break;
-            case "Description": bColor(1);
+            case "Description":
+                bColor(1);
                 fragment = new Description();
                 fragmentSet(fragmentTransaction, fragment);
                 break;
-            case "  Symbols  ": bColor(2);
+            case "  Symbols  ":
+                bColor(2);
                 fragment = new Symbols();
                 fragmentSet(fragmentTransaction, fragment);
                 break;
-            case " Biography ": bColor(3);
+            case " Biography ":
+                bColor(3);
                 fragment = new Biography();
                 fragmentSet(fragmentTransaction, fragment);
                 break;
-            case "  Actions  ": bColor(4);
+            case "  Actions  ":
+                bColor(4);
                 fragment = new Actions();
                 fragmentSet(fragmentTransaction, fragment);
                 break;
-            default: bColor(4);
+            default:
+                bColor(4);
                 break;
         }
     }
 
-    public void bColor(int index){
-        for(int i=0; i<5; i++){
-            if(i != index) {
+    public void bColor(int index) {
+        for (int i = 0; i < 5; i++) {
+            if (i != index) {
                 button[i].setBackgroundColor(Color.parseColor("#bae7b2b4"));
-            }else{
+            } else {
                 button[i].setBackgroundColor(Color.parseColor("#FF9F3032"));
             }
         }
     }
 
-    public void fragmentSet(FragmentTransaction fragmentTransaction, Fragment fragment){
+    public void fragmentSet(FragmentTransaction fragmentTransaction, Fragment fragment) {
         fragmentTransaction.replace(R.id.frame, fragment);
         fragmentTransaction.commit();
     }
-
 }
